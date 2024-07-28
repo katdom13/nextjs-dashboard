4b23ce6072fef76b2704d433ad567c5773319c06
katdom13 <katdom13@gmail.com>
2024-07-28 20:46:51 +0800

    Update customers page, add metadata

--- app/dashboard/(overview)/page.tsx
+++ app/dashboard/(overview)/page.tsx
@@ -5,7 +5,12 @@ import { fetchCardData } from '../../lib/data';
 import { Suspense } from 'react';
 import { CardsSkeleton, LatestInvoicesSkeleton, RevenueChartSkeleton } from '@/app/ui/skeletons';
 import CardWrapper from '@/app/ui/dashboard/cards';
+import { Metadata } from 'next/dist/types';
  
+export const metadata: Metadata = {
+  title: 'Dashboard',
+};
+
 export default async function Page() {
   const {
     totalPaidInvoices,
